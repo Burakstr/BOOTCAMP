145384cad43e7dc439f9451b7840d0b287b6d85a
Burak <145280474+Burakstr@users.noreply.github.com>
2023-09-27 12:24:11 +0300

    Add files via upload

---
+++ DAY 1/HOMEWORK1.playground/Contents.swift
@@ -0,0 +1,52 @@
+import UIKit
+
+var ilce = "Kağıthane"
+print("İlçe : \(ilce)")
+
+var kıta = "Avrupa"
+print("Kıta : \(kıta)")
+
+var faks = "0212"
+print("Faks : \(faks)")
+
+var postaKodu = 34000
+print("Posta Kodu : \(postaKodu)")
+
+var instaAdres = "Burakstrr"
+print("İnstagram adresi : \(instaAdres)")
+
+var calıstıgınBolum = "İos Developer"
+print("Çalıştığın Bölüm : \(calıstıgınBolum)")
+
+var urunMiktari = 381
+print("Ürün Miktarı : \(urunMiktari)")
+
+var musteriSoyadı = "Satır"
+print("Müşteri Soyadı : \(musteriSoyadı)")
+
+var odemeMiktari = 5555.55
+print("Ödeme Miktarı : \(odemeMiktari)")
+
+var dogumTarihi = "19.10.1999"
+print("Doğum Tarihi : \(dogumTarihi)")
+
+var borc = 6000.0
+print("Borc : \(borc)")
+
+var medeniHal = "Bekar"
+print("Medeni Hal : \(medeniHal)")
+
+var videoYorumu = "Yaralı bir video olmuş."
+print("Video Yorumu : \(videoYorumu)")
+
+var odemeSaati = "14:30"
+print("Ödeme Saati : \(odemeSaati)")
+
+var eftMiktari = "10.000 ₺"
+print("Eft Miktari : \(eftMiktari)")
+
+var satılanMiktar = 300
+print("Satılan Miktar : \(satılanMiktar)")
+
+var telefonModeli = "İphone 15 Pro"
+print("Telefon Modeli : \(telefonModeli)")
