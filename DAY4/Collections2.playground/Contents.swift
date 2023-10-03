d1899bd5fec755f808eb98dc99aaff666d2d1240
Burak <145280474+Burakstr@users.noreply.github.com>
2023-10-03 16:00:16 +0300

    Add files via upload

---
+++ DAY4/Collections2.playground/Contents.swift
@@ -0,0 +1,55 @@
+import UIKit
+
+//Set
+var meyveler = Set<String>()
+
+//Veri Ekleme
+meyveler.insert("Elma")
+meyveler.insert("Portakal")
+meyveler.insert("Muz")
+print(meyveler)
+
+meyveler.insert("Amasya Elması")
+print(meyveler)
+
+
+for meyve in meyveler {
+    print("Sonuç : \(meyve)")
+}
+
+for (indeks,meyve) in meyveler.enumerated() {
+    print("\(indeks). -> \(meyve)")
+}
+
+print("Boyut : \(meyveler.count)")
+print("Boş mu : \(meyveler.isEmpty)")
+
+meyveler.remove("Elma")
+print(meyveler)
+
+meyveler.removeAll()
+print(meyveler)
+
+//Dictinoary - Hashmap - Map
+
+var iller = [Int:String]()
+
+//Veri ekleme
+iller[16] = "Bursa"
+iller[34] = "İstanbul"
+
+print(iller)
+
+//Veri okuma
+print(iller[16]!)
+
+//Veri güncelleme
+iller[16] = "Yeni Bursa"
+print(iller)
+
+for (anahtar,deger) in iller {
+    print("\(anahtar) -> \(deger)")
+}
+
+iller.removeValue(forKey: 16)
+print(iller)
