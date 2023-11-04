70c5be0c6cc6a2ab684c78209d39a374b3c6f29a
Burak <145280474+Burakstr@users.noreply.github.com>
2023-11-04 11:55:04 +0300

    Add files via upload

---
+++ CalismaYapisi/CalismaYapisi/SonucEkraniVC.swift
@@ -0,0 +1,21 @@
+//
+//  SonucEkraniVC.swift
+//  CalismaYapisi
+//
+//  Created by Burak Satır on 30.09.2023.
+//
+
+import UIKit
+
+class SonucEkraniVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    @IBAction func buttonKapat(_ sender: Any) {
+        self.dismiss(animated: true)
+    }
+    
+}
